3112e407c3f5367105eb4742011fa80ff508f253
PCaam-git <pcaamano@outlook.es>
2026-05-13 10:59:44 +0200

    configurado TypeOrm con PostgreSql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ServersModule } from './servers/servers.module';
 import { ChannelsModule } from './channels/channels.module';
 import { MessagesModule } from './messages/messages.module';
+import { ServersModule } from './servers/servers.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    // NestJS lee las variables desde el archvo .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // Conexión con la base de datos PostgreSQL usando TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      // Auto carga de entidades y sincronización de la base de datos
+      autoLoadEntities: true,
+      // Se usa solo en desarrollo para crear automáticamente las tablas al modificar entidades. En producción, usar migraciones.
+      synchronize: true,
+    }),
     AuthModule,
     UsersModule,
     ServersModule,
