3948604287ebe3d299958caa58bdd8daeae74fee
PCaam-git <pcaamano@outlook.es>
2026-05-22 12:32:08 +0200

    añadido readme completo y actualizado y comentarios que permiten hacer más comprensible el código

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,8 +13,10 @@ import { AuthService } from './auth.service';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
+        // el secreto se recupera de las variables de entorno
         const jwtSecret = configService.get<string>('JWT_SECRET');
 
+        // si falta el secreto, se impide arrancar
         if (!jwtSecret) {
           throw new Error('JWT_SECRET no está definido en .env');
         }
@@ -22,6 +24,7 @@ import { AuthService } from './auth.service';
         return {
           secret: jwtSecret,
           signOptions: {
+            // tiempo de validez del access token emitido al usuario
             expiresIn: '1h',
           },
         };
